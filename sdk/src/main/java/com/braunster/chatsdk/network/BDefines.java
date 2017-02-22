a7e97649bc686e85b5a12ee9aed45d224c5128a7
simonsmiley <simon_smiley@hotmail.com>
2017-02-22 11:41:05 +1100

    switching Android to iOS Firebase path

--- sdk/src/main/java/com/braunster/chatsdk/network/BDefines.java
+++ sdk/src/main/java/com/braunster/chatsdk/network/BDefines.java
@@ -18,7 +18,7 @@ public class BDefines {
 
     /** This is the root path of all the app data,
      *  This is helpful if you want to test new behavior and don't want to infect all the old thread. */
-    public static final String BRootPath = "chat-sdk-v3/";
+    public static final String BRootPath = "firebase_v4_local/";
 
     /**
      * The version name of the app, Here we are using the name from the BuildConfig.
@@ -28,10 +28,10 @@ public class BDefines {
     public static String BAppVersion = BuildConfig.VERSION_NAME;
 
     /** The server url that is used to do all the API calls.*/
-    public static String ServerUrl = "https://chatcatv3test.firebaseio.com/" + BRootPath;
+    public static String ServerUrl = "https://chat-sdk-v4.firebaseio.com/" + BRootPath;
 
     /** The url that is used for the file uploads.*/
-    public static String FirebaseStoragePath = "gs://chatcatv3test.appspot.com";
+    public static String FirebaseStoragePath = "gs://chat-sdk-v4.appspot.com";
 
     /**
      * The name of the app image directory that will be seen in the phone image galley
