63675561ed038985420766a34b4435075969fdc1
Bahram Malaekeh <bahram.malaekeh@gmail.com>
2018-02-15 00:34:09 +0000

    - Java 8 compile option enabled, allowing for Lambda and Reference calls. Example class converted.

--- chat-sdk-firebase-adapter/src/main/java/co/chatsdk/firebase/FirebaseAuthenticationHandler.java
+++ chat-sdk-firebase-adapter/src/main/java/co/chatsdk/firebase/FirebaseAuthenticationHandler.java
@@ -1,10 +1,8 @@
 package co.chatsdk.firebase;
 
 import android.os.AsyncTask;
-import android.support.annotation.NonNull;
 
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -25,14 +23,8 @@ import co.chatsdk.core.types.AuthKeys;
 import co.chatsdk.core.types.ChatError;
 import co.chatsdk.firebase.wrappers.UserWrapper;
 import io.reactivex.Completable;
-import io.reactivex.CompletableEmitter;
-import io.reactivex.CompletableOnSubscribe;
 import io.reactivex.Single;
-import io.reactivex.SingleEmitter;
 import io.reactivex.SingleOnSubscribe;
-import io.reactivex.functions.Action;
-import io.reactivex.functions.Consumer;
-import io.reactivex.functions.Function;
 import io.reactivex.schedulers.Schedulers;
 
 import static co.chatsdk.firebase.FirebaseErrors.getFirebaseError;
@@ -44,140 +36,101 @@ import static co.chatsdk.firebase.FirebaseErrors.getFirebaseError;
 public class FirebaseAuthenticationHandler extends AbstractAuthenticationHandler {
 
     public Completable authenticateWithCachedToken() {
-        return Single.create(new SingleOnSubscribe<FirebaseUser>() {
-            @Override
-            public void subscribe(final SingleEmitter<FirebaseUser> e) throws Exception {
+        return Single.create((SingleOnSubscribe<FirebaseUser>)
+                emitter->{
+                    if (isAuthenticating()) {
+                        emitter.onError(ChatError.getError(ChatError.Code.AUTH_IN_PROCESS, "Cant execute two auth in parallel"));
+                    } else {
+                        setAuthStatus(AuthStatus.CHECKING_IF_AUTH);
 
-                if (isAuthenticating()) {
-                    e.onError(ChatError.getError(ChatError.Code.AUTH_IN_PROCESS, "Cant execute two auth in parallel"));
-                }
-                else {
-                    setAuthStatus(AuthStatus.CHECKING_IF_AUTH);
+                        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
-                    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-
-                    if (user != null) {
-                        e.onSuccess(user);
+                        if (user != null) {
+                            emitter.onSuccess(user);
+                        } else {
+                            emitter.onError(ChatError.getError(ChatError.Code.NO_AUTH_DATA, "No auth bundle found"));
+                        }
                     }
-                    else {
-                        e.onError(ChatError.getError(ChatError.Code.NO_AUTH_DATA, "No auth bundle found"));
-                    }
-                }
-            }
-        }).flatMapCompletable(new Function<FirebaseUser, Completable>() {
-            @Override
-            public Completable apply(FirebaseUser firebaseUser) throws Exception {
-                return authenticateWithUser(firebaseUser);
-            }
-        }).doOnTerminate(new Action() {
-            @Override
-            public void run() throws Exception {
-                // Whether we complete successfully or not, we set the status to idle
-                setAuthStateToIdle();
-            }
-        }).subscribeOn(Schedulers.single());
+                })
+                .flatMapCompletable(this::authenticateWithUser)
+                .doOnTerminate(this::setAuthStateToIdle) // Whether we complete successfully or not, we set the status to idle
+                .subscribeOn(Schedulers.single());
     }
 
     @Override
-    public Completable authenticate (final AccountDetails details) {
-        return Single.create(new SingleOnSubscribe<FirebaseUser>() {
-            @Override
-            public void subscribe(final SingleEmitter<FirebaseUser> e) throws Exception {
-
-                if (isAuthenticating())
-                {
-                    e.onError(ChatError.getError(ChatError.Code.AUTH_IN_PROCESS, "Can't execute two auth in parallel"));
-                    return;
-                }
-
-                setAuthStatus(AuthStatus.AUTH_WITH_MAP);
-
-                OnCompleteListener<AuthResult> resultHandler = new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull final Task<AuthResult> task) {
-                        AsyncTask.execute(new Runnable() {
-                            @Override
-                            public void run() {
-                                if(task.isComplete() && task.isSuccessful()) {
-                                    e.onSuccess(task.getResult().getUser());
-                                }
-                                else {
-                                    task.getException().printStackTrace();
-                                    e.onError(task.getException());
-                                }
-                            }
-                        });
+    public Completable authenticate(final AccountDetails details) {
+        return Single.create((SingleOnSubscribe<FirebaseUser>)
+                emitter->{
+                    if (isAuthenticating()) {
+                        emitter.onError(ChatError.getError(ChatError.Code.AUTH_IN_PROCESS, "Can't execute two auth in parallel"));
+                        return;
                     }
-                };
 
-                switch (details.type)
-                {
-                    case Username:
-                        FirebaseAuth.getInstance().signInWithEmailAndPassword(details.username, details.password).addOnCompleteListener(resultHandler);
-                        break;
-                    case  Register:
-                        FirebaseAuth.getInstance().createUserWithEmailAndPassword(details.username, details.password).addOnCompleteListener(resultHandler);
-                        break;
-                    case Anonymous:
-                        FirebaseAuth.getInstance().signInAnonymously().addOnCompleteListener(resultHandler);
-                        break;
-                    case Custom:
-                        FirebaseAuth.getInstance().signInWithCustomToken(details.token).addOnCompleteListener(resultHandler);
-                        break;
-                    // Should be handled by Social Login Module
-                    case Facebook:
-                    case Twitter:
-                    default:
-                        e.onError(ChatError.getError(ChatError.Code.NO_LOGIN_TYPE, "No matching login type was found"));
-                        break;
-                }
-            }
-        }).flatMapCompletable(new Function<FirebaseUser, Completable>() {
-            @Override
-            public Completable apply(FirebaseUser firebaseUser) throws Exception {
-                return authenticateWithUser(firebaseUser);
-            }
-        }).doOnTerminate(new Action() {
-            @Override
-            public void run() throws Exception {
-                // Whether we complete successfully or not, we set the status to idle
-                setAuthStateToIdle();
-            }
-        }).subscribeOn(Schedulers.single());
+                    setAuthStatus(AuthStatus.AUTH_WITH_MAP);
+
+                    OnCompleteListener<AuthResult> resultHandler = task->AsyncTask.execute(()->{
+                        if (task.isComplete() && task.isSuccessful()) {
+                            emitter.onSuccess(task.getResult().getUser());
+                        } else {
+                            task.getException().printStackTrace();
+                            emitter.onError(task.getException());
+                        }
+                    });
+
+                    switch ( details.type ) {
+                        case Username:
+                            FirebaseAuth.getInstance().signInWithEmailAndPassword(details.username, details.password).addOnCompleteListener(resultHandler);
+                            break;
+                        case Register:
+                            FirebaseAuth.getInstance().createUserWithEmailAndPassword(details.username, details.password).addOnCompleteListener(resultHandler);
+                            break;
+                        case Anonymous:
+                            FirebaseAuth.getInstance().signInAnonymously().addOnCompleteListener(resultHandler);
+                            break;
+                        case Custom:
+                            FirebaseAuth.getInstance().signInWithCustomToken(details.token).addOnCompleteListener(resultHandler);
+                            break;
+                        // Should be handled by Social Login Module
+                        case Facebook:
+                        case Twitter:
+                        default:
+                            emitter.onError(ChatError.getError(ChatError.Code.NO_LOGIN_TYPE, "No matching login type was found"));
+                            break;
+                    }
+                })
+                .flatMapCompletable(this::authenticateWithUser)
+                .doOnTerminate(this::setAuthStateToIdle)
+                .subscribeOn(Schedulers.single());
     }
 
-    public Completable authenticateWithUser (final FirebaseUser user) {
-        return Completable.create(new CompletableOnSubscribe() {
-            @Override
-            public void subscribe(final CompletableEmitter e) throws Exception {
+    public Completable authenticateWithUser(final FirebaseUser user) {
+        return Completable.create(
+                emitter->{
+                    final Map<String, Object> loginInfoMap = new HashMap<>();
+                    // Save the authentication ID for the current user
+                    // Set the current user
 
-                final Map<String, Object> loginInfoMap =  new HashMap<>();
-                // Save the authentication ID for the current user
-                // Set the current user
+                    String uid = user.getUid();
 
-                String uid = user.getUid();
+                    loginInfoMap.put(AuthKeys.CurrentUserID, uid);
 
-                loginInfoMap.put(AuthKeys.CurrentUserID, uid);
+                    setLoginInfo(loginInfoMap);
 
-                setLoginInfo(loginInfoMap);
+                    setAuthStatus(AuthStatus.HANDLING_F_USER);
 
-                setAuthStatus(AuthStatus.HANDLING_F_USER);
+                    // Do a once() on the user to push its details to firebase.
+                    final UserWrapper userWrapper = UserWrapper.initWithAuthData(user);
 
-                // Do a once() on the user to push its details to firebase.
-                final UserWrapper userWrapper = UserWrapper.initWithAuthData(user);
-
-                userWrapper.once().subscribe(new Action() {
-                    @Override
-                    public void run() throws Exception {
+                    userWrapper.once().subscribe(()->{
                         userWrapper.getModel().update();
 
                         FirebaseEventHandler.shared().currentUserOn(userWrapper.getModel().getEntityID());
 
-                        if(NM.push() != null) {
+                        if (NM.push() != null) {
                             NM.push().subscribeToPushChannel(userWrapper.pushChannel());
                         }
 
-                        if(NM.hook() != null) {
+                        if (NM.hook() != null) {
                             HashMap<String, Object> data = new HashMap<>();
                             data.put(BaseHookHandler.UserAuthFinished_User, userWrapper.getModel());
                             NM.hook().executeHook(BaseHookHandler.UserAuthFinished, data);
@@ -185,28 +138,13 @@ public class FirebaseAuthenticationHandler extends AbstractAuthenticationHandler
 
                         NM.core().setUserOnline().subscribe();
 
-                        userWrapper.push().subscribe(new Action() {
-                            @Override
-                            public void run() throws Exception {
-                                e.onComplete();
-                            }
-                        }, new Consumer<Throwable>() {
-                            @Override
-                            public void accept(Throwable throwable) throws Exception {
-                                throwable.printStackTrace();
-                            }
-                        });
-                    }
-                }, new Consumer<Throwable>() {
-                    @Override
-                    public void accept(Throwable throwable) throws Exception {
+                        userWrapper.push().subscribe(emitter::onComplete, Throwable::printStackTrace);
+                    }, throwable->{
                         throwable.printStackTrace();
-                        e.onError(throwable);
-                    }
-                });
-
-            }
-        }).subscribeOn(Schedulers.single());
+                        emitter.onError(throwable);
+                    });
+                })
+                .subscribeOn(Schedulers.single());
     }
 
     public Boolean userAuthenticated() {
@@ -215,112 +153,87 @@ public class FirebaseAuthenticationHandler extends AbstractAuthenticationHandler
 
     @Override
     public Completable changePassword(String email, String oldPassword, final String newPassword) {
-        return Completable.create(new CompletableOnSubscribe() {
-            @Override
-            public void subscribe(final CompletableEmitter e) throws Exception {
-                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        return Completable.create(
+                emitter->{
+                    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
-                OnCompleteListener<Void> resultHandler = new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Void> task) {
+                    OnCompleteListener<Void> resultHandler = task->{
                         if (task.isSuccessful()) {
-                            e.onComplete();
+                            emitter.onComplete();
                         } else {
-                            e.onError(getFirebaseError(DatabaseError.fromException(task.getException())));
+                            emitter.onError(getFirebaseError(DatabaseError.fromException(task.getException())));
                         }
-                    }
-                };
+                    };
 
-                user.updatePassword(newPassword).addOnCompleteListener(resultHandler);
-            }
-        }).subscribeOn(Schedulers.single());
+                    user.updatePassword(newPassword).addOnCompleteListener(resultHandler);
+                })
+                .subscribeOn(Schedulers.single());
     }
 
     public Completable logout() {
-        return Completable.create(new CompletableOnSubscribe() {
-            @Override
-            public void subscribe(final CompletableEmitter e) throws Exception {
+        return Completable.create(
+                emitter->{
+                    final User user = NM.currentUser();
 
-                final User user = NM.currentUser();
+                    // Stop listening to user related alerts. (added message or thread.)
+                    FirebaseEventHandler.shared().userOff(user.getEntityID());
 
-                // Stop listening to user related alerts. (added message or thread.)
-                FirebaseEventHandler.shared().userOff(user.getEntityID());
+                    // Removing the push channel
+                    if (NM.push() != null) {
+                        NM.push().unsubscribeToPushChannel(user.getPushChannel());
+                    }
 
-                // Removing the push channel
-                if (NM.push() != null) {
-                    NM.push().unsubscribeToPushChannel(user.getPushChannel());
-                }
-
-                NM.core().setUserOffline().subscribe(new Action() {
-                    @Override
-                    public void run() throws Exception {
+                    NM.core().setUserOffline().subscribe(()->{
 
                         FirebaseAuth.getInstance().signOut();
 
                         NM.events().source().onNext(NetworkEvent.logout());
 
-                        if(NM.socialLogin() != null) {
+                        if (NM.socialLogin() != null) {
                             NM.socialLogin().logout();
                         }
 
-                        if(NM.hook() != null) {
+                        if (NM.hook() != null) {
                             HashMap<String, Object> data = new HashMap<>();
                             data.put(BaseHookHandler.Logout_User, user);
                             NM.hook().executeHook(BaseHookHandler.Logout, data);
                         }
 
-                        e.onComplete();
-                    }
-                }, new Consumer<Throwable>() {
-                    @Override
-                    public void accept(Throwable throwable) throws Exception {
-                        e.onError(throwable);
-                    }
-                });
+                        emitter.onComplete();
+                    }, emitter::onError);
 
-            }
-        }).subscribeOn(Schedulers.single());
+                })
+                .subscribeOn(Schedulers.single());
     }
 
     public Completable sendPasswordResetMail(final String email) {
-        return Completable.create(new CompletableOnSubscribe() {
-            @Override
-            public void subscribe(final CompletableEmitter e) throws Exception {
-                OnCompleteListener<Void> resultHandler = new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Void> task) {
+        return Completable.create(
+                emitter->{
+                    OnCompleteListener<Void> resultHandler = task->{
                         if (task.isSuccessful()) {
-                            e.onComplete();
+                            emitter.onComplete();
                         } else {
-                            e.onError(getFirebaseError(DatabaseError.fromException(task.getException())));
+                            emitter.onError(getFirebaseError(DatabaseError.fromException(task.getException())));
                         }
-                    }
-                };
+                    };
 
-                FirebaseAuth.getInstance().sendPasswordResetEmail(email).addOnCompleteListener(resultHandler);
+                    FirebaseAuth.getInstance().sendPasswordResetEmail(email).addOnCompleteListener(resultHandler);
 
-            }
-        }).subscribeOn(Schedulers.single());
+                }).subscribeOn(Schedulers.single());
     }
 
     // TODO: Allow users to turn anonymous login off or on in settings
     public Boolean accountTypeEnabled(AccountDetails.Type type) {
-        if(type == AccountDetails.Type.Anonymous) {
+        if (type == AccountDetails.Type.Anonymous) {
             return ChatSDK.config().anonymousLoginEnabled;
-        }
-        else if(type == AccountDetails.Type.Username || type == AccountDetails.Type.Register) {
+        } else if (type == AccountDetails.Type.Username || type == AccountDetails.Type.Register) {
             return true;
-        }
-        else if(NM.socialLogin() != null) {
+        } else if (NM.socialLogin() != null) {
             return NM.socialLogin().accountTypeEnabled(type);
-        }
-        else {
+        } else {
             return false;
         }
     }
-
-
-
 
 
 }
