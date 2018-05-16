37577f312da75e31a2757187d975e758f10ff2e6
Pepe Becker <mail@pepebecker.com>
2018-05-17 00:07:49 +0100

    Save and display meta data

--- app/src/main/java/co/chatsdk/android/app/CustomPublicThreadEditDetailsActivity.java
+++ app/src/main/java/co/chatsdk/android/app/CustomPublicThreadEditDetailsActivity.java
@@ -2,17 +2,23 @@ package co.chatsdk.android.app;
 
 import android.content.Intent;
 import android.net.Uri;
-import android.os.Bundle;
 import android.util.Log;
 import android.webkit.MimeTypeMap;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import co.chatsdk.core.dao.ThreadMetaValue;
+import co.chatsdk.core.session.ChatSDK;
 import co.chatsdk.core.session.NM;
 import co.chatsdk.core.types.FileUploadResult;
 import co.chatsdk.core.utils.StringChecker;
+import co.chatsdk.firebase.wrappers.ThreadWrapper;
+import co.chatsdk.ui.manager.InterfaceManager;
 import co.chatsdk.ui.threads.PublicThreadEditDetailsActivity;
+import co.chatsdk.ui.utils.ToastHelper;
 import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
 import timber.log.Timber;
 
@@ -20,11 +26,31 @@ public class CustomPublicThreadEditDetailsActivity extends PublicThreadEditDetai
 
     final int CHOOSE_FILE = 42;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected EditText buildingInput;
+    protected EditText cityInput;
+    protected EditText pdfInput;
 
+    @Override
+    protected void initViews() {
         setContentView(R.layout.custom_activity_edit_public_thread_details);
+        super.initViews();
+
+        buildingInput = findViewById(R.id.chat_sdk_edit_thread_building_et);
+        cityInput = findViewById(R.id.chat_sdk_edit_thread_city_et);
+        pdfInput = findViewById(R.id.chat_sdk_edit_thread_pdf_et);
+
+        ThreadMetaValue buildingMetaValue = thread.metaValueForKey("building");
+        ThreadMetaValue cityMetaValue = thread.metaValueForKey("city");
+        ThreadMetaValue pdfMetaValue = thread.metaValueForKey("pdf");
+
+        if (buildingMetaValue != null)
+            buildingInput.setText(buildingMetaValue.getValue());
+
+        if (cityMetaValue != null)
+            cityInput.setText(cityMetaValue.getValue());
+
+        if (pdfMetaValue!= null)
+            pdfInput.setText(pdfMetaValue.getValue());
 
         Button uploadPDFButton = findViewById(R.id.chat_sdk_edit_thread_pdf_btn);
         uploadPDFButton.setOnClickListener(v -> {
@@ -33,6 +59,41 @@ public class CustomPublicThreadEditDetailsActivity extends PublicThreadEditDetai
             Intent chooser = Intent.createChooser(intent, "Choose a file");
             startActivityForResult(chooser, CHOOSE_FILE);
         });
+    }
+
+    @Override
+    protected void setSaveButtonOnClickListener() {
+        final String threadName = nameInput.getText().toString();
+        if (thread == null) {
+            showOrUpdateProgressDialog(getString(co.chatsdk.ui.R.string.add_public_chat_dialog_progress_message));
+
+            disposableList.add(NM.publicThread().createPublicThreadWithName(threadName)
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe((_thread, throwable) -> {
+                        if (throwable == null) {
+                            thread.setMetaValue("building", buildingInput.getText().toString());
+                            thread.setMetaValue("city", cityInput.getText().toString());
+                            thread.setMetaValue("pdf", pdfInput.getText().toString());
+                            disposableList.add(new ThreadWrapper(thread).pushMeta().subscribe(() -> {
+                                dismissProgressDialog();
+                                ToastHelper.show(ChatSDK.shared().context(), String.format(getString(co.chatsdk.ui.R.string.public_thread__is_created), threadName));
+
+                                InterfaceManager.shared().a.startChatActivityForID(ChatSDK.shared().context(), _thread.getEntityID());
+                            }));
+                        } else {
+                            ChatSDK.logError(throwable);
+                            Toast.makeText(ChatSDK.shared().context(), throwable.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                            dismissProgressDialog();
+                        }
+                    }));
+        } else {
+            thread.setName(threadName);
+            thread.update();
+            thread.setMetaValue("building", buildingInput.getText().toString());
+            thread.setMetaValue("city", cityInput.getText().toString());
+            thread.setMetaValue("pdf", pdfInput.getText().toString());
+            disposableList.add(new ThreadWrapper(thread).pushMeta().subscribe(this::finish));
+        }
     }
 
     @Override
--- app/src/main/java/co/chatsdk/android/app/CustomThreadDetailsActivity.java
+++ app/src/main/java/co/chatsdk/android/app/CustomThreadDetailsActivity.java
@@ -1,16 +1,43 @@
 package co.chatsdk.android.app;
 
-import android.os.Bundle;
+import android.widget.TextView;
 
+import co.chatsdk.core.dao.ThreadMetaValue;
+import co.chatsdk.core.events.EventType;
+import co.chatsdk.core.events.NetworkEvent;
+import co.chatsdk.core.session.NM;
 import co.chatsdk.ui.threads.ThreadDetailsActivity;
 
 public class CustomThreadDetailsActivity extends ThreadDetailsActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
+    protected void initViews() {
         setContentView(R.layout.custom_activity_thread_details);
+        super.initViews();
+
+        updateMetaData();
+
+        disposableList.add(NM.events().sourceOnMain()
+                .filter(NetworkEvent.filterType(EventType.ThreadMetaUpdated))
+                .subscribe(networkEvent -> updateMetaData()));
     }
 
+    private void updateMetaData() {
+        TextView buildingLabel = findViewById(R.id.chat_sdk_thread_building_tv);
+        TextView cityLabel = findViewById(R.id.chat_sdk_thread_city_tv);
+        TextView pdfLabel = findViewById(R.id.chat_sdk_thread_pdf_tv);
+
+        ThreadMetaValue buildingMetaValue = thread.metaValueForKey("building");
+        ThreadMetaValue cityMetaValue = thread.metaValueForKey("city");
+        ThreadMetaValue pdfMetaValue = thread.metaValueForKey("pdf");
+
+        if (buildingMetaValue != null)
+            buildingLabel.setText(buildingMetaValue.getValue());
+
+        if (cityMetaValue != null)
+            cityLabel.setText(cityMetaValue.getValue());
+
+        if (pdfMetaValue!= null)
+            pdfLabel.setText(pdfMetaValue.getValue());
+    }
 }
