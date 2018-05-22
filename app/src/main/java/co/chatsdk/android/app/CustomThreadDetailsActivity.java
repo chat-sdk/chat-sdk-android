971d7a5784832883c649c6adb0c2b433a3611d80
Pepe Becker <mail@pepebecker.com>
2018-05-22 15:34:31 +0100

    Change PDF link to PDF Attachment button

--- app/src/main/java/co/chatsdk/android/app/CustomThreadDetailsActivity.java
+++ app/src/main/java/co/chatsdk/android/app/CustomThreadDetailsActivity.java
@@ -1,11 +1,17 @@
 package co.chatsdk.android.app;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import co.chatsdk.core.dao.ThreadMetaValue;
 import co.chatsdk.core.events.EventType;
 import co.chatsdk.core.events.NetworkEvent;
+import co.chatsdk.core.session.ChatSDK;
 import co.chatsdk.core.session.NM;
 import co.chatsdk.ui.threads.ThreadDetailsActivity;
 
@@ -25,7 +31,7 @@ public class CustomThreadDetailsActivity extends ThreadDetailsActivity {
 
         TextView buildingLabel = findViewById(R.id.chat_sdk_thread_building_tv);
         TextView cityLabel = findViewById(R.id.chat_sdk_thread_city_tv);
-        TextView pdfLabel = findViewById(R.id.chat_sdk_thread_pdf_tv);
+        Button pdfButton = findViewById(R.id.pdf_attachment_btn);
 
         ThreadMetaValue buildingMetaValue = thread.metaValueForKey("building");
         ThreadMetaValue cityMetaValue = thread.metaValueForKey("city");
@@ -37,8 +43,17 @@ public class CustomThreadDetailsActivity extends ThreadDetailsActivity {
         if (cityMetaValue != null)
             cityLabel.setText(cityMetaValue.getValue());
 
-        if (pdfMetaValue != null)
-            pdfLabel.setText(pdfMetaValue.getValue());
+        if (pdfMetaValue != null && !pdfMetaValue.getValue().isEmpty()) {
+            pdfButton.setEnabled(true);
+            pdfButton.setVisibility(View.VISIBLE);
+            pdfButton.setOnClickListener(v -> {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(pdfMetaValue.getValue()));
+                startActivity(browserIntent);
+            });
+        } else {
+            pdfButton.setEnabled(false);
+            pdfButton.setVisibility(View.INVISIBLE);
+        }
     }
 
     @Override
