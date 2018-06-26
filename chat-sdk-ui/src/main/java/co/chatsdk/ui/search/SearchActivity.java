5a2396e57f8a4a73f29ae018d7024291ade93701
Pepe Becker <mail@pepebecker.com>
2018-06-26 23:53:42 +0100

    Exclude current user from contacts filtering

--- chat-sdk-ui/src/main/java/co/chatsdk/ui/search/SearchActivity.java
+++ chat-sdk-ui/src/main/java/co/chatsdk/ui/search/SearchActivity.java
@@ -70,7 +70,7 @@ public class SearchActivity extends BaseActivity {
         super.onNewIntent(intent);
     }
 
-    protected void initViews(){
+    protected void initViews() {
         searchImageView = findViewById(R.id.chat_sdk_btn_search);
         addContactsButton = findViewById(R.id.chat_sdk_btn_add_contacts);
         searchTextView = findViewById(R.id.chat_sdk_et_search_input);
@@ -124,8 +124,8 @@ public class SearchActivity extends BaseActivity {
 
             ArrayList<Completable> completables = new ArrayList<>();
 
-            for(UserListItem u : adapter.getSelectedUsers()) {
-                if(u instanceof User && !((User) u).isMe()) {
+            for (UserListItem u : adapter.getSelectedUsers()) {
+                if (u instanceof User && !((User) u).isMe()) {
                     completables.add(NM.contact().addContact((User) u, ConnectionType.Contact));
                 }
             }
@@ -179,7 +179,7 @@ public class SearchActivity extends BaseActivity {
                 @Override
                 public void onNext(@NonNull User user) {
 
-                    if(!existingContacts.contains(user)) {
+                    if (!existingContacts.contains(user) && !user.isMe()) {
                         users.add(user);
                         adapter.setUsers(users, true);
                         hideSoftKeyboard(SearchActivity.this);
@@ -196,7 +196,7 @@ public class SearchActivity extends BaseActivity {
                 @Override
                 public void onComplete() {
                     dialog.dismiss();
-                    if(users.size() == 0) {
+                    if (users.size() == 0) {
                         showToast(getString(R.string.search_activity_no_user_found_toast));
                     }
                 }
@@ -214,7 +214,7 @@ public class SearchActivity extends BaseActivity {
             final List<SearchActivityType> activities = new ArrayList<>(InterfaceManager.shared().a.getSearchActivities());
             activities.add(new SearchActivityType(InterfaceManager.shared().a.getSearchActivity(), context.getString(R.string.search_with_name)));
 
-            if(activities.size() == 1) {
+            if (activities.size() == 1) {
                 InterfaceManager.shared().a.startActivity(context, activities.get(0).className);
                 return;
             }
@@ -222,7 +222,7 @@ public class SearchActivity extends BaseActivity {
             String [] items = new String [activities.size()];
             int i = 0;
 
-            for(SearchActivityType activity : activities) {
+            for (SearchActivityType activity : activities) {
                 items[i++] = activity.title;
             }
 
