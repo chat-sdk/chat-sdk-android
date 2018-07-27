f3c7e5483b3882ee55c1db78a3317a9de07bd6e2
Pepe Becker <mail@pepebecker.com>
2018-07-27 19:22:16 +0700

    Add disposables list and cleanup code

--- chat-sdk-ui/src/main/java/co/chatsdk/ui/profile/EditProfileActivity.java
+++ chat-sdk-ui/src/main/java/co/chatsdk/ui/profile/EditProfileActivity.java
@@ -16,9 +16,7 @@ import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.facebook.drawee.view.SimpleDraweeView;
-import com.mukesh.countrypicker.Country;
 import com.mukesh.countrypicker.CountryPicker;
-import com.mukesh.countrypicker.OnCountryPickerListener;
 
 import org.apache.commons.lang3.StringUtils;
 
@@ -34,13 +32,13 @@ import co.chatsdk.core.defines.Availability;
 import co.chatsdk.core.session.ChatSDK;
 import co.chatsdk.core.session.InterfaceManager;
 import co.chatsdk.core.session.StorageManager;
+import co.chatsdk.core.utils.DisposableList;
 import co.chatsdk.core.utils.ImageUtils;
 import co.chatsdk.ui.R;
 import co.chatsdk.ui.chat.MediaSelector;
 import co.chatsdk.ui.main.BaseActivity;
 import id.zelory.compressor.Compressor;
 import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.disposables.Disposable;
 
 /**
  * Created by ben on 8/14/17.
@@ -62,6 +60,8 @@ public class EditProfileActivity extends BaseActivity {
 
     protected User currentUser;
 
+    private DisposableList disposableList = new DisposableList();
+
     @Override
     protected void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -69,7 +69,7 @@ public class EditProfileActivity extends BaseActivity {
 
         String userEntityID = getIntent().getStringExtra(InterfaceManager.USER_ENTITY_ID);
 
-        if(userEntityID == null || userEntityID.isEmpty()) {
+        if (userEntityID == null || userEntityID.isEmpty()) {
             showToast("User Entity ID not set");
             finish();
             return;
@@ -107,7 +107,7 @@ public class EditProfileActivity extends BaseActivity {
 
         avatarImageView.setOnClickListener(view -> mediaSelector.startChooseImageActivity(EditProfileActivity.this, MediaSelector.CropType.Circle,result -> {
 
-            try{
+            try {
                 File compress = new Compressor(ChatSDK.shared().context())
                         .setMaxHeight(ChatSDK.config().imageMaxThumbnailDimension)
                         .setMaxWidth(ChatSDK.config().imageMaxThumbnailDimension)
@@ -129,19 +129,16 @@ public class EditProfileActivity extends BaseActivity {
 
         avatarImageView.setImageURI(currentUser.getAvatarURL());
 
-        if (StringUtils.isNotEmpty(countryCode)){
+        if (StringUtils.isNotEmpty(countryCode)) {
             Locale l = new Locale("", countryCode);
             countryButton.setText(l.getDisplayCountry());
         }
 
         countryButton.setOnClickListener(view -> {
 
-            final CountryPicker picker = new CountryPicker.Builder().with(EditProfileActivity.this).listener(new OnCountryPickerListener() {
-                @Override
-                public void onSelectCountry(Country country) {
-                    countryButton.setText(country.getName());
-                    currentUser.setCountryCode(country.getCode());
-                }
+            final CountryPicker picker = new CountryPicker.Builder().with(EditProfileActivity.this).listener(country -> {
+                countryButton.setText(country.getName());
+                currentUser.setCountryCode(country.getCode());
             }).build();
 
             picker.showDialog(getSupportFragmentManager());
@@ -152,7 +149,7 @@ public class EditProfileActivity extends BaseActivity {
 
         statusEditText.setText(status);
 
-        if(!StringUtils.isEmpty(availability)) {
+        if (!StringUtils.isEmpty(availability)) {
             setAvailability(availability);
         }
 
@@ -164,21 +161,19 @@ public class EditProfileActivity extends BaseActivity {
     }
 
     protected void logout () {
-        Disposable d = ChatSDK.auth().logout()
+        disposableList.add(ChatSDK.auth().logout()
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(() -> ChatSDK.ui().startLoginActivity(getApplicationContext(), false), throwable -> {
             ChatSDK.logError(throwable);
             Toast.makeText(EditProfileActivity.this, throwable.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
-        });
+        }));
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
 
-        MenuItem item =
-                menu.add(Menu.NONE, R.id.action_chat_sdk_save, 12, getString(R.string.action_save));
+        MenuItem item = menu.add(Menu.NONE, R.id.action_chat_sdk_save, 12, getString(R.string.action_save));
         item.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
-//        item.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
         item.setIcon(R.drawable.icn_24_save);
 
         return true;
@@ -190,8 +185,7 @@ public class EditProfileActivity extends BaseActivity {
         /* Cant use switch in the library*/
         int id = item.getItemId();
 
-        if (id == R.id.action_chat_sdk_save)
-        {
+        if (id == R.id.action_chat_sdk_save) {
             saveAndExit();
             return true;
         }
@@ -202,7 +196,7 @@ public class EditProfileActivity extends BaseActivity {
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        try{
+        try {
             mediaSelector.handleResult(this, requestCode, resultCode, data);
         }
         catch (Exception e) {
@@ -228,7 +222,7 @@ public class EditProfileActivity extends BaseActivity {
         currentUser.setEmail(email);
 
         boolean changed = !userMeta.equals(currentUser.metaMap());
-        boolean imageChanged = false;
+//        boolean imageChanged = false;
         boolean presenceChanged = false;
 
         Map<String, Object> metaMap = new HashMap(currentUser.metaMap());
@@ -238,49 +232,47 @@ public class EditProfileActivity extends BaseActivity {
         // Add a synchronized block to prevent concurrent modification exceptions
         while (i.hasNext()) {
             String key = i.next();
-            if(key.equals(Keys.AvatarURL)) {
-                imageChanged = valueChanged(metaMap, userMeta, key);
+            if (key.equals(Keys.AvatarURL)) {
+//                imageChanged = valueChanged(metaMap, userMeta, key);
                 currentUser.setAvatarHash(null);
             }
-            if(key.equals(Keys.Availability) || key.equals(Keys.Status)) {
+            if (key.equals(Keys.Availability) || key.equals(Keys.Status)) {
                 presenceChanged = presenceChanged || valueChanged(metaMap, userMeta, key);
             }
         }
 
         currentUser.update();
 
-        if(presenceChanged && !changed) {
+        if (presenceChanged && !changed) {
             // Send presence
             ChatSDK.core().goOnline();
         }
 
         // TODO: Add this in for Firebase maybe move this to push user...
-//        if(imageChanged && avatarURL != null) {
+//        if (imageChanged && avatarURL != null) {
 //            UserAvatarHelper.saveProfilePicToServer(avatarURL, this).subscribe();
 //        }
 //        else if (changed) {
 
         final Runnable finished = () -> {
             View v = getCurrentFocus();
-            if(v instanceof EditText) {
+            if (v instanceof EditText) {
                 InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
-                imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                if (imm != null) imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
             }
 
             finish();
         };
 
 
-        if(changed) {
-
+        if (changed) {
             showOrUpdateProgressDialog(getString(R.string.alert_save_contact));
-
-            ChatSDK.core().pushUser()
+            disposableList.add(ChatSDK.core().pushUser()
                     .observeOn(AndroidSchedulers.mainThread())
                     .subscribe(() -> {
                         dismissProgressDialog();
                         finished.run();
-                    });
+                    }));
         }
         else {
             finished.run();
@@ -297,12 +289,11 @@ public class EditProfileActivity extends BaseActivity {
         }
     }
 
-    protected int getIndex(Spinner spinner, String myString)
-    {
+    protected int getIndex(Spinner spinner, String myString) {
         int index = 0;
 
-        for (int i=0;i<spinner.getCount();i++){
-            if (spinner.getItemAtPosition(i).toString().equalsIgnoreCase(myString)){
+        for (int i = 0; i<spinner.getCount(); i++) {
+            if (spinner.getItemAtPosition(i).toString().equalsIgnoreCase(myString)) {
                 index = i;
                 break;
             }
@@ -312,29 +303,27 @@ public class EditProfileActivity extends BaseActivity {
 
     protected String getAvailability () {
         String a = availabilitySpinner.getSelectedItem().toString().toLowerCase();
-        if(a.equals("away")) {
-            return Availability.Away;
-        }
-        else if(a.equals("extended away")) {
-            return Availability.XA;
-        }
-        else if(a.equals("busy")) {
-            return Availability.Busy;
-        }
-        else {
-            return Availability.Available;
+        switch (a) {
+            case "away":
+                return Availability.Away;
+            case "extended away":
+                return Availability.XA;
+            case "busy":
+                return Availability.Busy;
+            default:
+                return Availability.Available;
         }
     }
 
     protected void setAvailability (String a) {
         String availability = "available";
-        if(a.equals(Availability.Away)) {
+        if (a.equals(Availability.Away)) {
             availability = "away";
         }
-        else if(a.equals(Availability.XA)) {
+        else if (a.equals(Availability.XA)) {
             availability = "extended away";
         }
-        else if(a.equals(Availability.Busy)) {
+        else if (a.equals(Availability.Busy)) {
             availability = "busy";
         }
         availabilitySpinner.setSelection(getIndex(availabilitySpinner, availability));
