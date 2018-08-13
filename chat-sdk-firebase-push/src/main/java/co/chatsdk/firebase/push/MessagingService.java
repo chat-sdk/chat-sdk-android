1aab64e311e97d33f4a0c716b838b3433fdf9967
Ben Smiley-Andrews <ben@chatsdk.co>
2018-08-13 12:47:39 +0200

    - Remove allow backup option from module manifests - Improve push notification handling for Oreo

--- chat-sdk-firebase-push/src/main/java/co/chatsdk/firebase/push/MessagingService.java
+++ chat-sdk-firebase-push/src/main/java/co/chatsdk/firebase/push/MessagingService.java
@@ -1,14 +1,14 @@
 package co.chatsdk.firebase.push;
 
+import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Build;
-import android.support.v4.app.NotificationCompat;
-import android.support.v4.app.NotificationManagerCompat;
 import android.support.v4.app.TaskStackBuilder;
 
 import com.example.firebasepushnotifications.R;
@@ -59,31 +59,40 @@ public class MessagingService extends FirebaseMessagingService {
 
         Uri alarmSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
 
-        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this)
+//        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this)
+//                .setSmallIcon(pushIcon)
+//                .setContentTitle(title)
+//                .setContentText(body)
+//                .setPriority(NotificationCompat.PRIORITY_MAX)
+//                .setSound(alarmSound)
+//                .setAutoCancel(true)
+//                .setChannelId(ChatSDKMessageChannel);
+
+        Notification.Builder builder = new Notification.Builder(this)
                 .setSmallIcon(pushIcon)
                 .setContentTitle(title)
                 .setContentText(body)
-                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setPriority(Notification.PRIORITY_MAX)
                 .setSound(alarmSound)
                 .setAutoCancel(true);
+
 
         // Tap Action
         if (threadEntityID != null && threadEntityID.length() > 0) {
             Intent intent = new Intent(this, InterfaceManager.shared().a.getChatActivity());
             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
             intent.putExtra(BaseInterfaceAdapter.THREAD_ENTITY_ID, threadEntityID);
-//            PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
 
-//
             PendingIntent pendingIntent = TaskStackBuilder.create(this)
                     .addParentStack(InterfaceManager.shared().a.getMainActivity())
                     .addNextIntent(intent)
                     .getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
 
-            mBuilder.setContentIntent(pendingIntent);
+            builder.setContentIntent(pendingIntent);
         }
 
-        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+//        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+        NotificationManager manager = (NotificationManager) getBaseContext().getSystemService(Context.NOTIFICATION_SERVICE);
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             // Create the NotificationChannel, but only on API 26+ because
@@ -91,16 +100,16 @@ public class MessagingService extends FirebaseMessagingService {
             CharSequence name = getString(R.string.app_name);
             String description = getString(R.string.push_channel_name);
             NotificationChannel channel = new NotificationChannel(ChatSDKMessageChannel, name, NotificationManager.IMPORTANCE_HIGH);
+            channel.enableVibration(true);
             channel.setDescription(description);
-            // Register the channel with the system
-//            notificationManager.createNotificationChannel(channel);
+
+            if (manager != null) {
+                manager.createNotificationChannel(channel);
+            }
+
         }
 
-//                .addAction(R.drawable.icon, "Call", pIntent)
-//                .addAction(R.drawable.icon, "More", pIntent)
-//                .addAction(R.drawable.icon, "And more", pIntent).
-
-        //notificationManager.notify(0, mBuilder.build());
+        manager.notify(0, builder.build());
 
     }
 
