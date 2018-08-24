af2c251b7c8636574cfe770574e0b78e60732f5d
Pepe Becker <mail@pepebecker.com>
2018-08-24 14:51:12 +0700

    Add support to read file from Uri (#338)

--- chat-sdk-core/src/main/java/co/chatsdk/core/utils/FileUtils.java
+++ chat-sdk-core/src/main/java/co/chatsdk/core/utils/FileUtils.java
@@ -1,9 +1,16 @@
 package co.chatsdk.core.utils;
 
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.OpenableColumns;
+
 import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 import co.chatsdk.core.session.ChatSDK;
 
@@ -24,6 +31,46 @@ public class FileUtils {
             ChatSDK.logError(e);
         }
         return bytes;
+    }
+
+    public static byte [] fileToBytes (Context context, Uri uri) {
+        try {
+            InputStream inputStream = context.getContentResolver().openInputStream(uri);
+            if (inputStream != null) return readBytes(inputStream);
+        } catch (IOException e) {
+            ChatSDK.logError(e);
+        }
+        return null;
+    }
+
+    public static byte[] readBytes(InputStream inputStream) throws IOException {
+        ByteArrayOutputStream byteBuffer = new ByteArrayOutputStream();
+        int bufferSize = 1024;
+        byte[] buffer = new byte[bufferSize];
+        int len = 0;
+        while ((len = inputStream.read(buffer)) != -1) {
+            byteBuffer.write(buffer, 0, len);
+        }
+        return byteBuffer.toByteArray();
+    }
+
+    public static String getFileName(Context context, Uri uri) {
+        String result = null;
+        if (uri.getScheme().equals("content")) {
+            try (Cursor cursor = context.getContentResolver().query(uri, null, null, null, null)) {
+                if (cursor != null && cursor.moveToFirst()) {
+                    result = cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME));
+                }
+            }
+        }
+        if (result == null) {
+            result = uri.getPath();
+            int cut = result.lastIndexOf('/');
+            if (cut != -1) {
+                result = result.substring(cut + 1);
+            }
+        }
+        return result;
     }
 
 }
