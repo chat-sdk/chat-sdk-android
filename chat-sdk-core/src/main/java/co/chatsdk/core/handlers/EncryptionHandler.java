a75c64d7dc35efdad2c5bea6da42bc9027c3f731
Pepe Becker <mail@pepebecker.com>
2018-12-10 23:51:15 +0900

    Remove local modules and use jitpack instead

--- chat-sdk-core/src/main/java/co/chatsdk/core/handlers/EncryptionHandler.java
+++
@@ -1,9 +0,0 @@
-package co.chatsdk.core.handlers;
-
-import co.chatsdk.core.dao.Message;
-
-public interface EncryptionHandler {
-
-    void encrypt (Message message);
-    void decrypt (Message message);
-}
--- chat-sdk-core/src/main/java/co/chatsdk/core/handlers/Module.java
+++
@@ -1,9 +0,0 @@
-package co.chatsdk.core.handlers;
-
-/**
- * Created by SimonSmiley-Andrews on 01/05/2017.
- */
-
-public interface Module {
-    void activate();
-}
