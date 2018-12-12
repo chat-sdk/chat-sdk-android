562cdc2c85875a37a87f1aa023368ebdf098a153
Pepe Becker <mail@pepebecker.com>
2018-12-13 01:06:02 +0900

    Add social login

--- app/src/main/java/com/raymond/gossipgirl/AppObj.java
+++ app/src/main/java/com/raymond/gossipgirl/AppObj.java
@@ -9,6 +9,10 @@ import com.facebook.flipper.plugins.inspector.DescriptorMapping;
 import com.facebook.flipper.plugins.inspector.InspectorFlipperPlugin;
 import com.facebook.flipper.plugins.network.NetworkFlipperPlugin;
 import com.facebook.soloader.SoLoader;
+import com.google.firebase.auth.EmailAuthProvider;
+import com.google.firebase.auth.FacebookAuthProvider;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.auth.TwitterAuthProvider;
 
 import androidx.multidex.MultiDex;
 import androidx.multidex.MultiDexApplication;
@@ -19,6 +23,7 @@ import co.chatsdk.core.session.Configuration;
 import co.chatsdk.firebase.FirebaseNetworkAdapter;
 import co.chatsdk.firebase.file_storage.FirebaseFileStorageModule;
 import co.chatsdk.firebase.push.FirebasePushModule;
+import co.chatsdk.firebase.ui.FirebaseUIModule;
 import co.chatsdk.ui.manager.BaseInterfaceAdapter;
 
 /**
@@ -44,6 +49,8 @@ public class AppObj extends MultiDexApplication {
         Configuration.Builder config = new Configuration.Builder(context);
         config.firebaseRootPath("18_12_raymond_test");
         config.googleMaps("AIzaSyCwwtZrlY9Rl8paM0R6iDNBEit_iexQ1aE");
+        config.twitterLogin("Kqprq5b6bVeEfcMAGoHzUmB3I", "hPd9HCt3PLnifQFrGHJWi6pSZ5jF7kcHKXuoqB8GJpSDAlVcLq");
+        config.googleLogin("1088435112418-e3t77t8jl2ucs8efeqs72o696in8soui.apps.googleusercontent.com");
 
         try {
             ChatSDK.initialize(config.build(), new BaseInterfaceAdapter(context), new FirebaseNetworkAdapter());
@@ -54,6 +61,12 @@ public class AppObj extends MultiDexApplication {
 
         FirebaseFileStorageModule.activate();
         FirebasePushModule.activate();
+
+        FirebaseUIModule.activate(context,
+                EmailAuthProvider.PROVIDER_ID,
+                FacebookAuthProvider.PROVIDER_ID,
+                TwitterAuthProvider.PROVIDER_ID,
+                GoogleAuthProvider.PROVIDER_ID);
     }
 
     @Override
