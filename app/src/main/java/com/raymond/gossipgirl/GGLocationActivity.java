1037025631ba0a793b391e86cd79dd45225fe344
Pepe Becker <mail@pepebecker.com>
2018-12-13 02:29:52 +0900

    Add code to request location permissions

--- app/src/main/java/com/raymond/gossipgirl/GGLocationActivity.java
+++ app/src/main/java/com/raymond/gossipgirl/GGLocationActivity.java
@@ -1,7 +1,11 @@
 package com.raymond.gossipgirl;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.View;
 import android.view.WindowManager;
 
@@ -10,8 +14,10 @@ import com.firebase.ui.auth.ErrorCodes;
 import com.firebase.ui.auth.FirebaseUiException;
 import com.firebase.ui.auth.IdpResponse;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import co.chatsdk.core.session.ChatSDK;
 import co.chatsdk.core.utils.DisposableList;
 import co.chatsdk.firebase.ui.FirebaseUIModule;
@@ -20,6 +26,8 @@ import co.chatsdk.ui.utils.ToastHelper;
 import static co.chatsdk.firebase.ui.FirebaseUIModule.RC_SIGN_IN;
 
 public class GGLocationActivity extends AppCompatActivity {
+
+    private final int RC_LOCATION_PERMISSION = 10021;
 
     private DisposableList disposableList = new DisposableList();
 
@@ -31,16 +39,46 @@ public class GGLocationActivity extends AppCompatActivity {
     }
 
     public void didClickOnContinue(View v) {
-        startAuthenticationActivity();
+        requestPermissions();
     }
 
-    public void startAuthenticationActivity() {
+    public void onPermissionsGranted() {
         startActivityForResult(
                 AuthUI.getInstance()
                         .createSignInIntentBuilder()
                         .setAvailableProviders(FirebaseUIModule.shared().getIdps())
                         .build(),
                 RC_SIGN_IN);
+    }
+
+    void requestPermissions() {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            String[] permissions = new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION};
+            if (shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION)) {
+                requestPermissions(permissions, RC_LOCATION_PERMISSION);
+            } else {
+                Intent intent = new Intent();
+                intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                Uri uri = Uri.fromParts("package", this.getPackageName(), null);
+                intent.setData(uri);
+                this.startActivity(intent);
+            }
+        } else {
+            onPermissionsGranted();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == RC_LOCATION_PERMISSION) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                onPermissionsGranted();
+            } else {
+                requestPermissions();
+            }
+        }
     }
 
     @Override
