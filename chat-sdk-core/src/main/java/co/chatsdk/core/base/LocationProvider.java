f80249e00308d30b283dae2254d27f77d5349f8a
Pepe Becker <mail@pepebecker.com>
2019-03-11 19:03:46 +0700

    call requestLocationUpdates on UI thread to fix bug

--- chat-sdk-core/src/main/java/co/chatsdk/core/base/LocationProvider.java
+++ chat-sdk-core/src/main/java/co/chatsdk/core/base/LocationProvider.java
@@ -19,10 +19,8 @@ import co.chatsdk.core.session.ChatSDK;
 import co.chatsdk.core.utils.DisposableList;
 import co.chatsdk.core.utils.PermissionRequestHandler;
 import io.reactivex.Observable;
-import io.reactivex.ObservableEmitter;
 import io.reactivex.ObservableOnSubscribe;
 import io.reactivex.Single;
-import io.reactivex.SingleEmitter;
 import io.reactivex.SingleOnSubscribe;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.schedulers.Schedulers;
@@ -45,51 +43,45 @@ public class LocationProvider {
     }
 
     public Observable<Location> requestLocationUpdates(Activity activity, long interval, int distance) {
-        return requestLocationUpdates(interval)
+        return requestLocationUpdates(activity, interval)
                 .distinctUntilChanged((l1, l2) -> l1.distanceTo(l2) < distance);
     }
 
+    @SuppressLint("MissingPermission")
     public Observable<Location> requestLocationUpdates(Activity activity, long interval) {
         return PermissionRequestHandler.shared().requestLocationAccess(activity)
-                .andThen(requestLocationUpdates(interval));
-    }
-
-    public Single<Location> getLastLocation(Activity activity) {
-        return PermissionRequestHandler.shared().requestLocationAccess(activity)
-                .andThen(getLastLocation());
-    }
-
-     @SuppressLint("MissingPermission")
-    protected Observable<Location> requestLocationUpdates(long interval) {
-        return Observable.create((ObservableOnSubscribe<Location>) observable -> {
-            LocationRequest locationRequest = new LocationRequest();
-            locationRequest.setInterval(interval * 1000);
-            locationRequest.setFastestInterval(interval * 1000);
-            locationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
-            LocationCallback locationCallback = new LocationCallback() {
-                @Override
-                public void onLocationResult(LocationResult locationResult) {
-                    Location location = getMostAccurateLocation(locationResult.getLocations());
-                    if (location != null) {
-                        observable.onNext(location);
-                    }
-                }
-            };
-            locationClient.requestLocationUpdates(locationRequest, locationCallback, Looper.myLooper());
-        }).subscribeOn(Schedulers.single()).observeOn(AndroidSchedulers.mainThread());
+                .andThen((Observable.create((ObservableOnSubscribe<Location>) observable -> {
+                    LocationRequest locationRequest = new LocationRequest();
+                    locationRequest.setInterval(interval * 1000);
+                    locationRequest.setFastestInterval(interval * 1000);
+                    locationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+                    LocationCallback locationCallback = new LocationCallback() {
+                        @Override
+                        public void onLocationResult(LocationResult locationResult) {
+                            Location location = getMostAccurateLocation(locationResult.getLocations());
+                            if (location != null) {
+                                observable.onNext(location);
+                            }
+                        }
+                    };
+                    activity.runOnUiThread(() -> {
+                        locationClient.requestLocationUpdates(locationRequest, locationCallback, Looper.myLooper());
+                    });
+                })).subscribeOn(Schedulers.single()).observeOn(AndroidSchedulers.mainThread()));
     }
 
     @SuppressLint("MissingPermission")
-    protected Single<Location> getLastLocation() {
-        return Single.create((SingleOnSubscribe<Location>) single -> {
-                locationClient.getLastLocation().addOnSuccessListener(location -> {
-                    if (location != null) {
-                        single.onSuccess(location);
-                    } else {
-                        single.onError(new Error(context().getResources().getString(R.string.location_is_null)));
-                    }
-                }).addOnFailureListener(single::onError);
-        }).subscribeOn(Schedulers.single());
+    public Single<Location> getLastLocation(Activity activity) {
+        return PermissionRequestHandler.shared().requestLocationAccess(activity)
+                .andThen((Single.create((SingleOnSubscribe<Location>) single -> {
+                    locationClient.getLastLocation().addOnSuccessListener(location -> {
+                        if (location != null) {
+                            single.onSuccess(location);
+                        } else {
+                            single.onError(new Error(context().getResources().getString(R.string.location_is_null)));
+                        }
+                    }).addOnFailureListener(single::onError);
+                })).subscribeOn(Schedulers.single()));
     }
 
     public Location getMostAccurateLocation(List<Location> locations) {
