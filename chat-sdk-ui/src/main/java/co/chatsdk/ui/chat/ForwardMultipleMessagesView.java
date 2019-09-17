1d515cc930a15a71154678c09a04c3226d340958
thecmart <conrad@chatsdk.co>
2019-09-17 19:10:16 +0200

    Work in progress 2.

---
+++ chat-sdk-ui/src/main/java/co/chatsdk/ui/chat/ForwardMultipleMessagesView.java
@@ -0,0 +1,15 @@
+package co.chatsdk.ui.chat;
+
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class ForwardMultipleMessagesView extends LinearLayout {
+
+    protected ImageButton buttonSend;
+    protected ImageButton buttonBack;
+    protected TextView messagesSelected;
+    
+}
